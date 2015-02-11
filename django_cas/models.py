fde5119d35a4f097cda3ba2741e61362f2f0cfed
Michael C O'Connor <michael@mcoconnor.net>
2015-02-11 11:12:57 -0600

    Add backwards-compatible support for projects using custom user models (Django 1.5+).

--- django_cas/models.py
+++ django_cas/models.py
@@ -1,2 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib import auth
+
+def get_user_model():
+    '''
+    For Django < 1.5 backward compatibility
+    '''
+    if hasattr(auth, 'get_user_model'):
+        return auth.get_user_model()
+    else:
+        return auth.models.User
+
+User = get_user_model()
